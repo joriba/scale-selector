24cacd16a4e2fc3841ead70f5ea2cb8013f24808
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2025-01-18 16:08:42 +0100

    add minor scales

--- generateScales.js
+++ generateScales.js
@@ -2,8 +2,17 @@ const NOTES = [
     'C', 'D♭', 'D', 'E♭', 'E', 'F', 'G♭/F#', 'G', 'A♭', 'A', 'B♭', 'B'
 ]
 
+function scaleSet() {
+    let major = NOTES.map(note => note.concat(' Maj'));
+    let natural_minor = NOTES.map(note => note.concat(' Min (nat)'));
+    let harmonic_minor = NOTES.map(note => note.concat(' Min (harm)'));
+    let melodic_minor = NOTES.map(note => note.concat(' Min (mel)'));
+    let set = major.concat(natural_minor, harmonic_minor, melodic_minor);
+    return set;
+}
+
 function generateScales() {
-    let set = NOTES.map(note => note.concat('Maj'));
+    let set = scaleSet();
     let shuffled = set.map(value => ({ value, sort: Math.random() }))
         .sort((a, b) => a.sort - b.sort)
         .map(({value}) => value);
